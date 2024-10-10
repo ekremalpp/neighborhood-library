e22c610e155a472b58fe997f001e5a52e4b25c21
Ekrem <ealp@pgh.yearup.org>
2024-10-10 00:01:59 -0400

    updated

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -43,6 +43,51 @@ public class NeighborhoodLibrary {
         inventory[6] = new Book(7, "978-0-451-52818-9", "The Catcher in the Rye");
         inventory[7] = new Book(8, "978-0-7432-7356-6", "Brave New World");
         inventory[8] = new Book(9, "978-0-141-03440-3", "Crime and Punishment");
+        inventory[9] = new Book(10, "978-0-152-45661-3", "Jane EYRE");
+        inventory[10] = new Book(11, "978-0-141-03440-3", "Gurur ve On yargi");
+        inventory[11] = new Book(12, "978-0-451-52984-1", "Hayvan Çiftliği");
+        inventory[12] = new Book(13, "978-1-4416-3050-4", "Kayıp Zamanın İzinde");
+        inventory[13] = new Book(14, "978-0-141-03450-2", "Jane Eyre");
+        inventory[14] = new Book(15, "978-0-307-26931-3", "Gurur ve Ön Yargı");
+        inventory[15] = new Book(16, "978-0-7432-7356-7", "Küçük Kadınlar");
+        inventory[16] = new Book(17, "978-0-06-093546-7", "Savaş ve Barış");
+        inventory[17] = new Book(18, "978-0-14-044927-3", "İki Şehrin Hikayesi");
+        inventory[18] = new Book(19, "978-1-5011-9781-0", "On Küçük Zenci");
+        inventory[19] = new Book(20, "978-1-4516-2205-3", "Büyük Düşünme Gücü");
+    }
+
+    private static void showAvailableBooks(Scanner scanner){
+        System.out.println("Available Books");
+        for(Book book:inventory){
+            if(!book.isCheckedOut()) {
+                System.out.println("ID:" + book.getId()+",ISBN:"+ book.getIsbn()+"Heading:" + book.getTitle());
+
+            }
+        }
+        String input = scanner.nextLine();
+        if(!input.equalsIgnoreCase("X")) {
+            int id =Integer.parseInt(input);
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+            for(Book book:inventory) {
+                if(book.getId()==id && !book.isCheckedOut()) {
+                    book.checkOut(name);
+                    System.out.println(book.getTitle()+"Browwed successfully");
+                    return;
+                }
+            }
+            System.out.println("Invalid ID or book already borrowed");
+        }
 
     }
+    private static void showCheckedOutBooks(Scanner scanner) {
+        System.out.println(" Broweed Books:");
+        for(Book book:inventory) {
+            if(book.isCheckedOut()) {
+                System.out.println("ID:" + book.getId() + "ISBNE:"+book.getIsbn()+"Heading" +book.getTitle() + "Borrower"+book.getCheckedOutTo());
+
+            }
+        }
+    }
+
 }
